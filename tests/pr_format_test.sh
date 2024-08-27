537a0baecef44da9f29a030abbba92f97b8acf1c
Chemaclass <chemaclass@outlook.es>
2024-08-27 16:35:55 +0200

    fix: get_ticket_number_lower_upper_case

--- tests/pr_format_test.sh
+++ tests/pr_format_test.sh
@@ -65,6 +65,10 @@ function test_get_ticket_number_with_prefix() {
   assert_equals "123" "$(get_ticket_number "feat/TICKET-123-my-branch_name")"
 }
 
+function test_get_ticket_number_lower_upper_case() {
+  assert_equals "123" "$(get_ticket_number "Ticket-123-my-branch_name")"
+}
+
 function test_get_ticket_key_default() {
   assert_equals "TICKET" "$(get_ticket_key "TICKET-123-my-branch_name")"
 }
