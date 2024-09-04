aab7d787beaee36773b257ec4a73f8036cce784e
Chemaclass <jose.valera@purpose-green.com>
2024-09-04 15:45:57 +0200

    test: format_pr_body_with_branch_with_numbers_with and without prefix

--- tests/format_pr_body_test.sh
+++ tests/format_pr_body_test.sh
@@ -52,7 +52,15 @@ function test_format_pr_body_without_pr_template() {
   assert_equals "" "$actual"
 }
 
-function test_format_pr_body_with_branch_with_numbers() {
+function test_format_pr_body_with_branch_with_numbers_no_prefix() {
+  export PR_TICKET_LINK_PREFIX=https://your-company.atlassian.net/browse/
+
+  local actual=$(format_pr_body "TICKET-123-my-4-th-branch" "$ROOT_DIR/.github/PULL_REQUEST_TEMPLATE.md")
+
+  assert_contains "https://your-company.atlassian.net/browse/TICKET-123" "$actual"
+}
+
+function test_format_pr_body_with_branch_with_numbers_with_prefix() {
   export PR_TICKET_LINK_PREFIX=https://your-company.atlassian.net/browse/
 
   local actual=$(format_pr_body "feat/TICKET-123-my-4-th-branch" "$ROOT_DIR/.github/PULL_REQUEST_TEMPLATE.md")
