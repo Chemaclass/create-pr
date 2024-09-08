bc19ea50b92182e5f527aed4d15bc6e67d42e6ef
Chemaclass <chemaclass@outlook.es>
2024-09-08 13:10:30 +0200

    test: add snapshots to pr_body_tests

--- tests/unit/pr_body_test.sh
+++ tests/unit/pr_body_test.sh
@@ -6,58 +6,41 @@ function set_up() {
   source "$CREATE_PR_ROOT_DIR/src/pr_body.sh"
 }
 
-function test_pr_body_link_without_comment() {
-  export PR_TICKET_LINK_PREFIX=https://your-ticket-system.com/
-
-  local actual=$(pr_body "TICKET-123-my_branch-with-1-number"\
-    "$CREATE_PR_ROOT_DIR/.github/PULL_REQUEST_TEMPLATE.md")
-
-  # validate that the link is not inside a HTML comment
-  assert_not_contains "<!-- https://your-ticket-system.com/TICKET-123 -->" "$actual"
-  # but the link is there
-  assert_contains "https://your-ticket-system.com/TICKET-123" "$actual"
-  # still comments might exists
-  assert_contains "<!-- " "$actual"
-}
-
 function test_pr_body_link_with_PR_TICKET_LINK_PREFIX() {
   export PR_TICKET_LINK_PREFIX=https://your-ticket-system.com/
 
   local actual=$(pr_body "TICKET-123-my_branch-with-1-number"\
     "$CREATE_PR_ROOT_DIR/.github/PULL_REQUEST_TEMPLATE.md")
 
-  assert_contains "https://your-ticket-system.com/TICKET-123" "$actual"
-  assert_not_contains "{{ TICKET_LINK }}" "$actual"
+  assert_match_snapshot "$actual"
 }
 
-function test_pr_body_link_without_PR_TICKET_LINK_PREFIX() {
+function test_pr_body_link_without_ticket_link_prefix() {
   export PR_TICKET_LINK_PREFIX=
 
   local actual=$(pr_body "TICKET-123-my_branch"\
     "$CREATE_PR_ROOT_DIR/.github/PULL_REQUEST_TEMPLATE.md")
 
-  assert_not_contains "https://your-ticket-system.com/" "$actual"
-  assert_not_contains "{{ TICKET_LINK }}" "$actual"
-  assert_contains "Nope" "$actual"
+  assert_match_snapshot "$actual"
 }
 
-function test_pr_body_link_without_ticket_key_default_link_prefix_text() {
+function test_pr_body_link_without_ticket_key() {
   export PR_TICKET_LINK_PREFIX=https://your-ticket-system.com/
 
   local actual=$(pr_body "123-my_branch"\
     "$CREATE_PR_ROOT_DIR/.github/PULL_REQUEST_TEMPLATE.md")
 
-  assert_contains "https://your-ticket-system.com/123" "$actual"
+  assert_match_snapshot "$actual"
 }
 
-function test_pr_body_link_custom_link_prefix_text() {
+function test_pr_body_link_with_pr_ticket_prefix_text() {
   export PR_TICKET_LINK_PREFIX=https://your-ticket-system.com/
   export PR_TICKET_PREFIX_TEXT="Fixes: "
 
   local actual=$(pr_body "123-my_branch"\
     "$CREATE_PR_ROOT_DIR/.github/PULL_REQUEST_TEMPLATE.md")
 
-  assert_contains "Fixes: https://your-ticket-system.com/123" "$actual"
+  assert_match_snapshot "$actual"
 }
 
 function test_pr_body_link_without_ticket_number() {
@@ -66,9 +49,7 @@ function test_pr_body_link_without_ticket_number() {
   local actual=$(pr_body "TICKET-my_branch"\
     "$CREATE_PR_ROOT_DIR/.github/PULL_REQUEST_TEMPLATE.md")
 
-  assert_not_contains "https://your-ticket-system.com/" "$actual"
-  assert_not_contains "Closes: Nope" "$actual"
-  assert_contains "Nope" "$actual"
+  assert_match_snapshot "$actual"
 }
 
 function test_pr_body_link_without_pr_template() {
@@ -85,27 +66,16 @@ function test_pr_body_link_with_branch_with_numbers_no_prefix() {
   local actual=$(pr_body "TICKET-123-my-4-th-branch"\
     "$CREATE_PR_ROOT_DIR/.github/PULL_REQUEST_TEMPLATE.md")
 
-  assert_contains "https://your-ticket-system.com/TICKET-123" "$actual"
+  assert_match_snapshot "$actual"
 }
 
-function test_pr_body_link_with_branch_with_numbers_with_prefix() {
+function test_pr_body_link_with_branch_with_numbers_and_prefix() {
   export PR_TICKET_LINK_PREFIX=https://your-ticket-system.com/
 
   local actual=$(pr_body "feat/TICKET-123-my-4-th-branch"\
     "$CREATE_PR_ROOT_DIR/.github/PULL_REQUEST_TEMPLATE.md")
 
-  assert_contains "https://your-ticket-system.com/TICKET-123" "$actual"
-}
-
-function test_pr_body_background_with_link() {
-  export PR_TICKET_LINK_PREFIX=https://your-ticket-system.com/
-
-  local actual=$(pr_body "feat/TICKET-123-my-4-th-branch"\
-    "$CREATE_PR_ROOT_DIR/.github/PULL_REQUEST_TEMPLATE.md")
-
-  assert_not_contains "{{ BACKGROUND }}" "$actual"
-  assert_not_contains "<!-- Details in the ticket. -->" "$actual"
-  assert_contains "Details in the ticket." "$actual"
+  assert_match_snapshot "$actual"
 }
 
 function test_pr_body_background_without_link() {
@@ -114,7 +84,5 @@ function test_pr_body_background_without_link() {
   local actual=$(pr_body "feat/TICKET-123-my-4-th-branch"\
     "$CREATE_PR_ROOT_DIR/.github/PULL_REQUEST_TEMPLATE.md")
 
-  assert_not_contains "{{ BACKGROUND }}" "$actual"
-  assert_not_contains "<!-- Provide some context to the reviewer before jumping in the code. -->" "$actual"
-  assert_contains "Provide some context to the reviewer before jumping in the code." "$actual"
+  assert_match_snapshot "$actual"
 }
