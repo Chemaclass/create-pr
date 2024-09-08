e6574d07a591f91b2280cb1dc7fdd44e053feab8
Chemaclass <chemaclass@outlook.es>
2024-09-08 12:55:01 +0200

    fix: add missing env to e2e test

--- tests/e2e/create-pr_test.sh
+++ tests/e2e/create-pr_test.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 
 function set_up() {
+  export PR_TEMPLATE_DIR=".github/PULL_REQUEST_TEMPLATE.md"
+  export PR_TICKET_LINK_PREFIX="https://github.com/Chemaclass/create-pr.sh/issues/"
+  export PR_TICKET_PREFIX_TEXT="Closes: "
+  export PR_LABEL_MAPPING="docs:documentation;\
+    feat|feature:enhancement;\
+    fix|bug|bugfix|hotfix:bug;\
+    default:enhancement"
+
   SCRIPT="$CREATE_PR_ROOT_DIR/create-pr.sh"
 }
 
