a057fb8979a397322286591a0dfda3a345872d6a
Chemaclass <chemaclass@outlook.es>
2024-09-15 15:02:24 +0200

    feat: use EXTRA_ARGS when creating pr github/gitlab

--- src/main.sh
+++ src/main.sh
@@ -20,12 +20,16 @@ function main::create_pr() {
 
 function main::create_pr_gitlab() {
   validate_glab_cli_is_installed
+  : "${EXTRA_ARGS:=()}"
+
   if glab mr create --title "$PR_TITLE" \
                       --target-branch "$BASE_BRANCH" \
                       --source-branch "$BRANCH_NAME" \
                       --assignee "$PR_ASSIGNEE" \
                       --label "$PR_LABEL" \
-                      --description "$PR_BODY"; then
+                      --description "$PR_BODY" \
+                      "${EXTRA_ARGS[@]}" \
+  ; then
     echo "Merge Request created successfully."
   else
     error_and_exit "Failed to create the Merge Request." \
@@ -35,13 +39,17 @@ function main::create_pr_gitlab() {
 
 function main::create_pr_github() {
   validate_gh_cli_is_installed
+  : "${EXTRA_ARGS:=()}"
+
   # Create the PR with the specified options
   if ! gh pr create --title "$PR_TITLE" \
                     --base "$BASE_BRANCH" \
                     --head "$BRANCH_NAME" \
                     --assignee "$PR_ASSIGNEE" \
                     --label "$PR_LABEL" \
-                    --body "$PR_BODY"; then
+                    --body "$PR_BODY" \
+                    "${EXTRA_ARGS[@]}" \
+  ; then
       error_and_exit "Failed to create the pull request."\
         "Ensure you have the correct permissions and the repository is properly configured."
   fi
