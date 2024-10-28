321de24923cb17a0a6c3e1cc5ab9c215816c0f2c
Chemaclass <chemaclass@outlook.es>
2024-10-28 13:34:07 +0100

    refactor: pr_ticket_number_test.sh
    using individual tests instead of data_provider

--- tests/unit/pr_ticket_number_test.sh
+++ tests/unit/pr_ticket_number_test.sh
@@ -5,7 +5,11 @@ function set_up() {
   source "$CREATE_PR_ROOT_DIR/src/pr_ticket.sh"
 }
 
-function test_pr_ticket_number_default() {
+function test_pr_ticket_number_start_with_number() {
+  assert_same "123" "$(pr_ticket::number "123-TICKET-my-branch_name")"
+}
+
+function test_pr_ticket_number_after_ticket_key() {
   assert_same "123" "$(pr_ticket::number "TICKET-123-my-branch_name")"
 }
 
@@ -29,17 +33,14 @@ function test_pr_ticket_number_with_numbers_in_branch_name() {
   assert_same "123" "$(pr_ticket::number "Ticket-123-my-2-nd-branch_name")"
 }
 
-# data_provider provide_ticket_number_only_at_the_beginning
-function test_pr_ticket_number_only_at_the_beginning() {
-  local branch_name="$1"
-
-  assert_empty "$(pr_ticket::number "$branch_name")"
+function test_pr_ticket_number_without_number() {
+  assert_empty "$(pr_ticket::number "creating-my-branch_name")"
 }
 
-function provide_ticket_number_only_at_the_beginning() {
-  echo "creating-my-branch_name"
-  echo "creating-my-2-nd-branch_name"
-  echo "TICKET-my-2-nd-branch_name"
-  echo "feat/creating-my-2-nd-branch_name"
-  echo "feat/TICKET-my-2-nd-branch_name"
+function test_pr_ticket_number_with_number_not_in_first_nor_second_pos() {
+  assert_empty "$(pr_ticket::number "creating-my-2-nd-branch_name")"
+}
+
+function test_pr_ticket_number_with_prefix_and_number_not_in_first_nor_second_pos() {
+  assert_empty "$(pr_ticket::number "feat/creating-my-2-nd-branch_name")"
 }
